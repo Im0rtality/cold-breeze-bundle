eb1bc007461257d697a8830c6ad9fa2261986a5f
Laurynas Veržukauskas <laurynas.verzukauskas@gmail.com>
2014-05-05 22:41:39 +0300

    Implemented Statistics endpoint

--- src/Im0rtality/ColdBreezeBundle/Helper/Statistics.php
+++ src/Im0rtality/ColdBreezeBundle/Helper/Statistics.php
@@ -2,22 +2,61 @@
 
 namespace Im0rtality\ColdBreezeBundle\Helper;
 
+use Sylius\Bundle\CoreBundle\Doctrine\ORM\OrderRepository;
+use Sylius\Bundle\CoreBundle\Doctrine\ORM\UserRepository;
+use Sylius\Component\Order\Model\Order;
+
 class Statistics
 {
+    /** @var OrderRepository */
+    protected $orderRepository;
+    /** @var UserRepository */
+    protected $userRepository;
 
     /**
      * @return array
      */
     public function getStatisticalData()
     {
-        return [
-            'title'                => 'month',
-            'averageOrderValue'    => 1,
-            'revenue'              => 1,
-            'ordersPerActiveUser'  => 1,
-            'revenuePerActiveUser' => 1,
-            'orders'               => 1,
-            'activeUsers'          => 1,
-        ];
+        $endDate   = new \DateTime();
+        $startDate = new \DateTime('-30 days');
+
+        $orders = $this->orderRepository->findBetweenDates($startDate, $endDate);
+        $users  = $this->userRepository
+            ->createQueryBuilder('u')
+            ->where('u.lastLogin >= :lastLogin')
+            ->setParameter('lastLogin', $startDate)
+            ->getQuery()
+            ->getResult();
+
+        $revenue = function ($sum, Order $value) {
+            return $sum + $value->getTotal();
+        };
+
+        $stats                         = ['title' => 'month'];
+        $stats['activeUsers']          = count($users);
+        $stats['orders']               = count($orders);
+        $stats['revenue']              = array_reduce($orders, $revenue, 0);
+        $stats['averageOrderValue']    = round($stats['revenue'] / $stats['orders'], 2);
+        $stats['ordersPerActiveUser']  = round($stats['orders'] / $stats['activeUsers'], 2);
+        $stats['revenuePerActiveUser'] = round($stats['revenue'] / $stats['activeUsers'], 2);
+
+        return $stats;
+    }
+
+    /**
+     * @param OrderRepository $orderRepository
+     */
+    public function setOrderRepository($orderRepository)
+    {
+        $this->orderRepository = $orderRepository;
+    }
+
+    /**
+     * @param UserRepository $userRepository
+     */
+    public function setUserRepository($userRepository)
+    {
+        $this->userRepository = $userRepository;
     }
 }
